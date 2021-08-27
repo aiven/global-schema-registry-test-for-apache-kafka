462c9c881ff122b5d36601e56e73decc58771d5b
Chris McConnell <chris.mcconnell@aiven.io>
2021-08-27 12:59:13 -0600

    initial commit

--- bin/destroy-endpoint-integration.sh
+++ bin/destroy-endpoint-integration.sh
@@ -16,8 +16,6 @@ AVN_KAFKA2_SVC_NAME=$(AWK -F "= " '/avn_kafka2_svc_name/ {print $2}' ${TFVARS} |
 
 # get project/service creds
 AVN_KAFKA1_SVC_PASSWORD="$(avn service user-list --format '{username} {password}' --project $PROJECT_NAME $AVN_KAFKA1_SVC_NAME | awk '{print $2}')"
-echo "AVN_KAFKA1_SVC_PASSWORD: $AVN_KAFKA1_SVC_PASSWORD"
-echo "PROJECT_NAME: $PROJECT_NAME"
 
 # get the endpoint ID from here
 ENDPOINT_ID=`avn service integration-endpoint-list --project $PROJECT_NAME | grep $ENDPOINT_NAME | awk '{print $1}'`
